84622934b504ca516381dac409d4baf05867ff94
chipmuenk <mail@chipmuenk.de>
2016-04-12 08:18:54 +0200

    Loesche nicht benötigten Code Signed-off-by: chipmuenk <mail@chipmuenk.de>

--- code/3_DFT/Plots/DFT_plot_NxN.py
+++ code/3_DFT/Plots/DFT_plot_NxN.py
@@ -63,132 +63,87 @@ t = linspace(0,1,num=NFFT*OSR) # "analog" time 0 ... 1 in NFFT*OSR steps
 y = ((n - 3)  > 0) * exp(-(n-2)/5)
 yt = exp(-((t -2)  > 0)*(t-3)/5)
 
+fig1, axes = plt.subplots(nrows=4, ncols=4)
 
+#print(axes)
+for i, ax in enumerate(axes.flat, start=1):
+    ax.set_title('Test Axes {}'.format(i))
+    ax.set_xlabel('X axis')
+    ax.set_ylabel('Y axis')
 
 fig1 = plt.figure(num=1, figsize=FIGSIZE)
 ax1 = fig1.add_subplot(111)
 
-ax1.grid(False)
-#
+from matplotlib.patches import FancyArrow
+import matplotlib.gridspec as gridspec
 
-#
-#ax1.set_xlabel(r'$n \rightarrow$')
-#ax1.xaxis.set_label_coords(2, 0.5, transform=ax1.transData)
-#ax1.set_ylabel(r'$x[n] \rightarrow$')
-#ax1.yaxis.set_label_coords(-0.3, 2.3, transform=ax1.transData)
-#ax1.set_ylim([-2,3])
-#ax1.set_ylim([-2,2.5])
-#
+def dftmatrix(Nfft=32,N=None):
+    'construct DFT matrix'
+    k= np.arange(Nfft)
+    if N is None: N = Nfft
+    n = arange(N)
+    U = np.matrix(exp(1j* 2*pi/Nfft *k*n[:,None])) # use numpy broadcasting to create matrix
+    return U/sqrt(Nfft)
 
-if SCALE:
-    ax1.spines['top'].set_visible(False)
-    ax1.spines['right'].set_visible(False)
-    ax1.spines['left'].set_visible(False)
-    ax1.get_yaxis().set_ticks([])
-    #ax1.yaxis.set_ticks_position('none')
-    #ax1.spines['left'].set_position(('data',0))
-    #ax1.spines['left'].set_linewidth(2)
-    
-    ax1.xaxis.set_ticks_position('bottom')
-    ax1.set_xticklabels([])
-    ax1.spines['bottom'].set_position(('data',0))
-    ax1.spines['bottom'].set_linewidth(2)
-    ax1.spines['bottom'].set_zorder(0)
-    ax1.tick_params(axis='x', direction='inout', width=2, length=10,
-                           color='k', labelsize=20, pad=5) # 'inout' 'in'
-#[tick.set_zorder(0) for tick in ax1.xaxis.ticklabels]
-#[line.set_zorder(1) for line in ax1.lines]
-#resadjust(ax1, yres = 1)
-#for label in ax1.get_xticklabels():
-#       label.set_horizontalalignment('left')
-    ax1.spines['bottom'].set_position(('data',0))
+Nfft=16
+v = ones((16,1))
+U = dftmatrix(Nfft=Nfft,N=16)
 
-if DISCRETE_T:
-    markerline, stemlines, baseline = ax1.stem(n[0:NDISP*NFFT], y[0:NDISP*NFFT])
-    plt.setp(markerline, 'markerfacecolor', 'r', 'markersize', 8, 'marker', 'o')
-    plt.setp(stemlines, 'color','b', 'linewidth', 2)
-    plt.setp(baseline, 'linewidth', 0) # turn off baseline
-    ax1.set_xlabel(r'$n, \;  \rightarrow$', size = 24, ha ="right")
-    ax1.xaxis.set_label_coords(1, 0.35)# transform=ax1.transData)   
-    if DEBUG: plot(t, yt,'b')
-else:
-    plot(n[0:NDISP*NFFT],y[0:NDISP*NFFT],'r',linewidth=3)
-    ax1.set_xlabel(r'$t \; \rightarrow$', size = 24, ha ="right")
-    ax1.xaxis.set_label_coords(1, 0.35)# transform=ax1.transData)   
-    if DEBUG: plot(t, yt, 'b')
-#for label in ax1.get_yticklabels():
-#       label.set_verticalalignment('bottom')
-#ax1.set_xticklabels(xticklabels, rotation=0, ha='left', minor=False)
-#
+# --- 
+# hardcoded constants to format complicated figure
 
-ax1.set_ylim(-0.3, 1.1)
-#plt.margins(0.02) # setting xmargin / ymargin individually doesnt work
-fig1.tight_layout()
+gs = gridspec.GridSpec(8,12)
+gs.update( wspace=1, left=0.01)
 
-#ax1.spines['left'].set_smart_bounds(True)
-#ax1.spines['bottom'].set_smart_bounds(True)
-#ax1.set_title(r'Faltung $y[n] = x[n] \star \{1; 1; 1; 1; 1\}$')
+fig =figure(figsize=(10,5))
+ax0 = subplot(gs[:,:3])
+fig.add_subplot(ax0)
 
-#plt.ticklabel_format(useOffset=False, axis='y') # disable using offset print
-fig1.savefig(BASE_DIR + FILENAME + '_t.'+FMT)
+ax0.set_aspect(1)
+a=2*pi/Nfft*arange(Nfft)
 
-#################### Spectrum ################################################
-fig2 = figure(num=2, figsize=FIGSIZE)
+colors = ['k','b','r','m','g','Brown','DarkBlue','Tomato','Violet', 'Tan','Salmon','Pink',
+          'SaddleBrown', 'SpringGreen', 'RosyBrown','Silver',]
+for j,i in enumerate(a):
+  ax0.add_patch( FancyArrow(0,0,cos(i),sin(i),width=0.02,
+                            length_includes_head=True,edgecolor=colors[j]))
 
-ax2 = fig2.add_subplot(111)
+ax0.text(1,0.1,'0',fontsize=16)
+ax0.text(0.1,1,r'$\frac{\pi}{2}$',fontsize=22)
+ax0.text(-1,0.1,r'$\pi$',fontsize=18)
+ax0.text(0.1,-1.2,r'$\frac{3\pi}{2}$',fontsize=22)
+ax0.axis(array([-1,1,-1,1])*1.45)
+ax0.set_title('Radial Frequency')
+ax0.set_xlabel('Real')
+ax0.set_ylabel('Imaginary')
 
-ax2.spines['top'].set_visible(False)
-#ax2.spines['right'].set_visible(False)
-ax2.spines['left'].set_visible(False)
-ax2.get_yaxis().set_ticks([])
-#ax2.spines['right'].set_position(('data',0))
-#ax1.yaxis.set_ticks_position('none')
-#ax1.spines['left'].set_position(('data',0))
-#ax1.spines['left'].set_linewidth(2)
+# plots in the middle column
+for i in range(8):
+  ax=subplot(gs[i,4:8])
+  ax.set_xticks([]);  ax.set_yticks([])
+  ax.set_ylabel(r'$\Omega_{%d}=%d\times\frac{2\pi}{16}$'%(i,i),fontsize=16,
+                rotation='horizontal')
+  ax.plot(U.real[:,i],'-o',color=colors[i])
+  ax.plot(U.imag[:,i],'--o',color=colors[i],alpha=0.2)
+  ax.axis(ymax=4/Nfft*1.1,ymin=-4/Nfft*1.1)
+ax.set_xticks(arange(16))
+ax.set_xlabel('n')
 
-ax2.xaxis.set_ticks_position('bottom')
-#ax2.set_xticklabels([])
-ax2.spines['bottom'].set_position(('data',0))
-ax2.spines['bottom'].set_linewidth(2)
-ax2.spines['bottom'].set_zorder(0)
-ax2.spines['right'].set_position(('data',0))
-ax2.spines['right'].set_linewidth(2)
-ax2.spines['right'].set_zorder(0)
-ax2.tick_params(axis='x', direction='inout', width=2, length=10,
-                       color='k', labelsize=20, pad=5) # 'inout' 'in'
+# plots in the far right column
+for i in range(8):
+  ax=subplot(gs[i,8:])
+  ax.set_xticks([]);  ax.set_yticks([])
+  ax.set_ylabel(r'$\Omega_{%d}=%d\times\frac{2\pi}{16}$'%(i+8,i+8),fontsize=16,
+                rotation='horizontal')
+  ax.plot(U.real[:,i+8],'-o',color=colors[i+8])
+  ax.plot(U.imag[:,i+8],'--o',color=colors[i+8],alpha=0.2)
+  ax.axis(ymax=4/Nfft*1.1,ymin=-4/Nfft*1.1)    
+  ax.yaxis.set_label_position('right')
+ax.set_xticks(arange(16))
+ax.set_xlabel('n')
 
-
-if ZEROPAD:
-    print(NPAD)
-    print(np.zeros(NPAD))
-    yn = np.concatenate((y, np.zeros(NPAD)))
-else:
-    yn = y
-    
-#Y = fft(yn)[0:len(yn)/2]/len(y)
-NNFFT = len(yn)
-NZOOM = int(NNFFT * 1/3)
-Y = fftshift(fft(yn))[NZOOM:NNFFT-NZOOM]
-print(len(Y), len(yn))
-YM = np.abs(Y)
-
-
-F = fftshift(fftfreq(len(YM)))*NDISP
-    
-markerline, stemlines, baseline = ax2.stem(F, YM, 'r')
-plt.setp(markerline, 'markerfacecolor', 'b', 'markersize', 8, 'marker', 'o')
-plt.setp(stemlines, 'color','r', 'linewidth', 2)
-plt.setp(baseline, 'linewidth', 0) # turn off baseline
-
-ax2.set_xlabel(r'$k \; \rightarrow$', size = 24, ha='right')
-
-#ax2.set_ylabel(r'$|H(\mathrm{e}^{\mathrm{j} 2 \pi F})| \rightarrow$')
-
-ax2.xaxis.set_label_coords(1, 0.4)
-ax2.set_xlim(-NDISP/2, NDISP/2)
-ax2.set_ylim(0, max(YM)*1.05)
-fig2.tight_layout()
-fig2.savefig(BASE_DIR + FILENAME + '_f.' + FMT)
+#fig2.tight_layout()
+#fig2.savefig(BASE_DIR + FILENAME + '_f.' + FMT)
 
 #def resadjust(ax, xres=None, yres=None):
 #    """
