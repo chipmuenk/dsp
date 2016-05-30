d42776442d2353ddaf82da6254baed4a224da0d2
chipmuenk <mail@chipmuenk.de>
2016-05-30 23:10:18 +0200

    minimum working example Signed-off-by: chipmuenk <mail@chipmuenk.de>

--- code/9_INP/plots/svg_exp_bug.py
+++ code/9_INP/plots/svg_exp_bug.py
@@ -4,10 +4,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-BASE_DIR = "/home/muenker/Daten/HM/dsvFPGA/Vorlesung/2016ss/nologo/img/"
-#BASE_DIR = "D:/Daten/HM/dsvFPGA/Vorlesung/2016ss/nologo/img/"
+#BASE_DIR = "/home/muenker/Daten/HM/dsvFPGA/Vorlesung/2016ss/nologo/img/"
+BASE_DIR = "D:/Daten/HM/dsvFPGA/Vorlesung/2016ss/nologo/img/"
 FILENAME = "svg_exp_test"
-FMT = ".svg"
+FMT = ".png"
 
 fig1 = plt.figure(num=1)
 ax1 = fig1.add_subplot(111)
@@ -21,7 +21,6 @@ ax1.set_ylim(0.3, 0.9)
 ax1.set_xlabel("x label")
 ax1.set_ylabel("y label")
 ax1.set_title("Title")
-#fig1.tight_layout()
 
 fig1.savefig(BASE_DIR + FILENAME + FMT)
 
