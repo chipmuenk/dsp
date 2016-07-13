42719006b9ff80f89f625e068ecf66e2a94abd11
Chipmuenk <mail@chipmuenk.de>
2016-07-13 22:43:38 +0200

    Comment and import cleanup, fixed integer index depracation warnings Signed-off-by: Chipmuenk <mail@chipmuenk.de>

--- code/08_SMP/plots/SMP_sampled_sine.py
+++ code/08_SMP/plots/SMP_sampled_sine.py
@@ -1,27 +1,27 @@
 # -*- coding: utf-8 -*-
-# SMP_sampled_sine.py ================================================
-# Plotte mehrere Sinusfunktionen unterschiedlicher Frequenz und Phase,
-# die alle die gleiche abgetastete Sequenz liefern (-> Aliasing!)
-# TODO: DFT der ursprünglichen und abgetasteten Signale
-# (c) 2013-Dez-2 Christian Münker - Files zur Vorlesung "DSV auf FPGAs"
-#%%=======================================================================
-from __future__ import division, print_function
+"""
+ === SMP_sampled_sine.py ================================================
+ 
+ Plotte mehrere Sinusfunktionen unterschiedlicher Frequenz und Phase,
+ die alle die gleiche abgetastete Sequenz liefern (-> Aliasing!)
+ 
+ TODO: DFT der ursprünglichen und abgetasteten Signale
+ 
+ (c) 2013-Dez-2 Christian Münker - Files zur Vorlesung "DSV auf FPGAs"
+ =======================================================================
+ """
+from __future__ import division, print_function, unicode_literals
 
 import numpy as np
-import numpy.random as rnd
+
 from numpy import sin, cos, tan, angle, pi, array, arange, log10, zeros, \
   linspace, ones, sqrt
 from numpy.fft import fft, ifft, fftshift, ifftshift, fftfreq
-import scipy.signal as sig
-import scipy.interpolate as intp
 
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import figure, plot, stem, grid, xlabel, ylabel, \
     subplot, title, clf, xlim, ylim
 
-import dsp_fpga_lib as dsp
-#------------------------------------------------------------------------
-# ... Ende der Import-Anweisungen
 f_1 = 500;    f_2 = 1000;    f_3 = 2500
 phi_1 = pi/4; phi_2 = -pi/4; phi_3 = -pi/4
 A1 = 1.; A2 = 1.; A3 = 1.
@@ -30,21 +30,22 @@ OSR = 1.5  # Oversampling Ratio in Bezug auf f_1
 
 t_min = 0; t_max = t_min + Np / f_1 
 N = 120; # Anzahl Datenpunkte pro Periode von f_1
+N_tot = int(Np * N) # Datenpunkte insgesamt
 # Erzeuge Vektor mit Np*N aequidistanten Zeitpunkten (Np Perioden von f_1):
 t  = linspace(t_min, t_max, Np*N+1)
 #
 
-NS = np.floor(N / (2 * OSR)) # Abtastung alle NS Zeitpunkte
-t_S = t[0:Np*N:NS] # Vektor mit Sampling-Zeitpunkten
+NS = int(np.floor(N / (2 * OSR))) # Abtastung alle NS Zeitpunkte
+t_S = t[0:N_tot:NS] # Vektor mit Sampling-Zeitpunkten
 f_S = 2 * f_1 * OSR # Abtastfrequenz
 #t_P =  t(1 : NS/8 : 2*N); 
 x1 = A1 * cos(f_1*2*pi*t + phi_1)
 x2 = A2 * cos(f_2*2*pi*t + phi_2)
 x3 = A3 * cos(f_3*2*pi*t + phi_3)
 # Abtasten der drei Funktionen
-x1_S = x1[0:Np*N:NS]
-x2_S = x2[0:Np*N:NS]
-x3_S = x3[0:Np*N:NS]
+x1_S = x1[0:N_tot:NS]
+x2_S = x2[0:N_tot:NS]
+x3_S = x3[0:N_tot:NS]
 #%% Figure 1: Zeitbereich
 fig1 = plt.figure(1) # neue Grafik
 plt.clf()
