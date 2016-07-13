da0eeb38e37e5268142b52f99bb4b278302ca9f0
Chipmuenk <mail@chipmuenk.de>
2016-07-13 08:30:28 +0200

    Comment cleanup, improved annotation Signed-off-by: Chipmuenk <mail@chipmuenk.de>

--- code/05_FIL/FIL_Linphase_Filter_annotate.py
+++ code/05_FIL/FIL_Linphase_Filter_annotate.py
@@ -1,34 +1,25 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*
-#===========================================================================
-# FIL-Linphase_Filter_annotate_py.py
-#
-# Demonstriere annotate() und text()
-#
-# 
-# 
-# 
-# 
-# 
-# (c) 2014-Feb-04 Christian Münker - Files zur Vorlesung "DSV auf FPGAs"
-#===========================================================================
-from __future__ import division, print_function, unicode_literals # v3line15
+# -*- coding: utf-8 -*-
+"""
+=== FIL_Linphase_Filter_annotate.py =======================================
+  
+ Demonstriere Einfluss von einfachen und doppelten Nullstellen im SB und DB,
+ mit Anmerkungen im Plot (annotate() und text() )
+ 
+ (c) 2016 Christian Münker - Files zur Vorlesung "DSV auf FPGAs"
+===========================================================================
+"""
+
+from __future__ import division, print_function, unicode_literals
 
 import numpy as np
-import numpy.random as rnd
 from numpy import (pi, log10, exp, sqrt, sin, cos, tan, angle, arange,
                     linspace, array, zeros, ones)
-from numpy.fft import fft, ifft, fftshift, ifftshift, fftfreq
 import scipy.signal as sig
-import scipy.interpolate as intp
 
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import (figure, plot, stem, grid, xlabel, ylabel,
     subplot, title, clf, xlim, ylim)
 
-import dsp_fpga_lib as dsp
-#------------------------------------------------------------------ v3line30
-# Ende der gemeinsamen Import-Anweisungen
 f_S = 1
 bb = [-1/8, 0, 5/8, 1, 5/8, 0, -1/8]
 aa = 1
@@ -40,8 +31,7 @@ print('------------------------------------------------')
 for i in range(len(z_0)):
     print('{0:2d} | {1:15.4f} |{2:6.3f} * e^(j {3:6.4f} pi) '\
     .format(i, z_0[i],abs(z_0[i]),np.angle(z_0[i])/pi))  
-#    print('%2d | %15.4f |%6.3f * e^(j {%6.4f} pi)'\
-#    %(i, z_0[i],abs(z_0[i]),np.angle(z_0[i])/pi))
+
 
 for i in range(len(z_0)):
     if z_0[i].real > 0: # Entferne Nullstellen im Durchlassband
@@ -75,7 +65,21 @@ subplot(212)
 plot(f,abs(H_neu)); grid(True)
 xlabel(r'$F \; \rightarrow$')
 
-plt.text( 0.1,12, "ohne Nullstellen auf pos. reeller Achse (DB)", 
+plt.text( 0.1,12, "ohne Nullstellen auf pos. reeller Achse\n (anderer Maßstab!)", 
             size=16, va="center", ha="left")
-            
+plt.annotate("einfache",
+            xy=(0.36, 0.05), xycoords='data',
+            xytext=(0.6, 0.5), textcoords='axes fraction',
+            size=16, va="center", ha="center",
+            arrowprops=dict(arrowstyle="simple",
+                            connectionstyle="arc3,rad=-0.2"),
+                            )
+plt.annotate("doppelte\n Nullstelle",
+            xy=(0.5, 0.05), xycoords='data',
+            xytext=(0.85, 0.5), textcoords='axes fraction',
+            size=16, va="center", ha="right",
+            arrowprops=dict(arrowstyle="simple",
+                            connectionstyle="arc3,rad=-0.2"),
+                            )
+# try 'figure fraction','axes points', 'offset points', ...             
 plt.tight_layout(); plt.show()
